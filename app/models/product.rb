72166d71518980b103551a491917c05ad5ef654c
justus <jmbaluka@mfarm.co.ke>
2015-05-08 20:36:57 +0300

    begun working on orders feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
 	validates_numericality_of :price
-	validates_numericality_of :stock, greater_than_or_equal_to: 0
+	validates_numericality_of :stock, only_integer: true , greater_than_or_equal_to: 0
+	has_many :order_items
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :orders
+  resources :order_items
   resources :products
  
+ root :to => "products#index"
 end
